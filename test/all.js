dddec7c2ab759aadc885379f0b135e11d25656f2
Nicita01 <nicitamac@gmail.com>
2018-09-10 15:48:44 +0300

    Update main file

--- test/all.js
+++ test/all.js
@@ -1,15 +1,10 @@
 'use strict';
 
-const all = [
-  'adapters', 'all', 'chain', 'collectors',
-  'array.each', 'array.every', 'array.filter',
-  'array.find', 'array.map', 'array.reduce',
-  'array.series', 'array.some',
-  'compose.clone', 'compose', 'compose.then',
-  'control', 'do', 'firstOf', 'memoize',
-  'fp.ap', 'fp.asAsync', 'fp.concat', 'fp.fmap', 'fp.of',
-  'poolify', 'throttle', 'queue', 'queue.modes'
-];
-
-all.forEach(name => require('./' + name));
-
+const fs = require('fs');
+const path = require('path');
+fs.readdir(__dirname, (err, files) => {
+  if (err) return console.error('Failed to run tests: ', err);
+  const thisFilename = path.basename(__filename);
+  files.filter(file => file !== thisFilename)
+    .forEach(name => require('./' + name));
+});
